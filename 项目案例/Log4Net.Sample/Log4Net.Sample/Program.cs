f8e17a6ad30c784f138b35119b92b1452856bed5
Aaron <Aaron@DESKTOP-UPNI5DP>
2019-11-13 19:38:02 +0800

    Log4Net、Quartz.Net

---
+++ 项目案例/Log4Net.Sample/Log4Net.Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Log4Net.Utils;
+
+namespace Log4Net.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start...");
+
+            //注册log4net
+            Logger.Instance().Register();
+            Logger.Info("hello word...");
+
+            Console.WriteLine("End...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 项目案例/Log4Net.Sample/Log4Net.Utils/LoggerMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net.Utils
+{
+    /// <summary>
+    /// 日志内容
+    /// </summary>
+    public class LoggerMessage
+    {
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 日志等级
+        /// </summary>
+        public LoggerLevel Level { get; set; }
+        /// <summary>
+        /// 异常信息
+        /// </summary>
+        public Exception Exception { get; set; }
+
+    }
+}
