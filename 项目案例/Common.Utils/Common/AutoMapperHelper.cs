d0b6c1a6436432c2b96ce5efaa90e3542f978dae
Aaron <969340992@qq.com>
2019-11-24 10:57:34 +0800

    新增

---
+++ 项目案例/Common.Utils/Common/AutoMapperHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Common
+{
+    /// <summary>
+    /// AutoMapper帮助类
+    /// </summary>
+    public static partial class AutoMapperHelper
+    {
+        /// <summary>
+        /// 集合对集合
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static List<TResult> MapTo<TResult>(this IEnumerable self)
+        {
+            if (self == null)
+                throw new ArgumentNullException();
+            return (List<TResult>)Mapper.Map(self, self.GetType(), typeof(List<TResult>));
+        }
+
+        /// <summary>
+        /// 对象对对象
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static TResult MapTo<TResult>(this object self)
+        {
+            if (self == null)
+                throw new ArgumentNullException();
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap(self.GetType(), typeof(TResult));
+            });
+            return (TResult)Mapper.Map(self, self.GetType(), typeof(TResult));
+        }
+    }
+}
