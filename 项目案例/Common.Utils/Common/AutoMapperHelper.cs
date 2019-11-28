eb3ca28dfe2129fce8911919fb58f482013c9431
Aaron <969340992@qq.com>
2019-11-28 20:10:00 +0800

    edit

--- 项目案例/Common.Utils/Common/AutoMapperHelper.cs
+++ 项目案例/Common.Utils/Common/AutoMapperHelper.cs
@@ -8,39 +8,71 @@ using AutoMapper;
 
 namespace Common
 {
-    /// <summary>
-    /// AutoMapper帮助类
-    /// </summary>
-    public static partial class AutoMapperHelper
+    public static class AutoMapperHelper
     {
         /// <summary>
-        /// 集合对集合
+        ///  类型映射,默认字段名字一一对应
         /// </summary>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="self"></param>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="obj"></param>
         /// <returns></returns>
-        public static List<TResult> MapTo<TResult>(this IEnumerable self)
+        public static TDestination MapTo<TDestination>(this object obj)
         {
-            if (self == null)
-                throw new ArgumentNullException();
-            return (List<TResult>)Mapper.Map(self, self.GetType(), typeof(List<TResult>));
+            if (obj == null) return default(TDestination);
+            var config = new AutoMapper.MapperConfiguration(cfg => cfg.CreateMap(obj.GetType(), typeof(TDestination)));
+            return config.CreateMapper().Map<TDestination>(obj);
         }
 
         /// <summary>
-        /// 对象对对象
+        /// 类型映射,默认字段名字一一对应
         /// </summary>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="self"></param>
-        /// <returns></returns>
-        public static TResult MapTo<TResult>(this object self)
+        /// <typeparam name="TSource">源数据：要被转化的实体对象</typeparam>
+        /// <typeparam name="TDestination">目标数据：转换后的实体对象</typeparam>
+        /// <param name="source">任何引用类型对象</param>
+        /// <returns>转化之后的实体</returns>
+        public static TDestination MapTo<TSource, TDestination>(this TSource source)
+            where TDestination : class
+            where TSource : class
         {
-            if (self == null)
-                throw new ArgumentNullException();
-            Mapper.Initialize(cfg =>
-            {
-                cfg.CreateMap(self.GetType(), typeof(TResult));
-            });
-            return (TResult)Mapper.Map(self, self.GetType(), typeof(TResult));
+            if (source == null) return default(TDestination);
+            var config = new AutoMapper.MapperConfiguration(cfg => cfg.CreateMap<TSource, TDestination>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<TDestination>(source);
+        }
+
+        /// <summary>
+        /// 类型映射,可指定映射字段的配置信息
+        /// </summary>
+        /// <typeparam name="TSource">源数据：要被转化的实体对象</typeparam>
+        /// <typeparam name="TDestination">目标数据：转换后的实体对象</typeparam>
+        /// <param name="source">任何引用类型对象</param>
+        /// <param name="cfgExp">可为null，则自动一一映射</param>
+        /// <returns></returns>
+        public static TDestination MapTo<TSource, TDestination>(this TSource source, Action<AutoMapper.IMapperConfigurationExpression> cfgExp)
+         where TDestination : class
+         where TSource : class
+        {
+            if (source == null) return default(TDestination);
+            var config = new AutoMapper.MapperConfiguration(cfgExp != null ? cfgExp : cfg => cfg.CreateMap<TSource, TDestination>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<TDestination>(source);
+        }
+
+        /// <summary>
+        /// 集合列表类型映射,默认字段名字一一对应
+        /// </summary>
+        /// <typeparam name="TDestination">转化之后的实体对象，可以理解为viewmodel</typeparam>
+        /// <typeparam name="TSource">要被转化的实体对象，Entity</typeparam>
+        /// <param name="source">通过泛型指定的这个扩展方法的类型，理论任何引用类型</param>
+        /// <returns>转化之后的实体列表</returns>
+        public static IEnumerable<TDestination> MapToList<TSource, TDestination>(this IEnumerable<TSource> source)
+            where TDestination : class
+            where TSource : class
+        {
+            if (source == null) return new List<TDestination>();
+            var config = new AutoMapper.MapperConfiguration(cfg => cfg.CreateMap<TSource, TDestination>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<List<TDestination>>(source);
         }
     }
 }
